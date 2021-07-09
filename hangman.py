d62060ff2b47e52b9d133b86a471d0ccd623dd5d
lowee1 <63391242+lowee1@users.noreply.github.com>
2021-07-09 17:55:33 +0800

    added basic graphics for tui created bare structure for other features

--- hangman.py
+++ hangman.py
@@ -1,8 +1,53 @@
 import click
+from colorama import init
 from random import choice
+import tui_graphics
 
-with open("popular.txt", "r") as file:
+init()
+
+with open("words_alpha.txt", "r") as file:
     words = file.readlines()
 chosen_word = choice(words)
 
-click.echo(chosen_word)
+
+@click.group()
+def main():
+    pass
+
+
+@main.group()
+def tui():
+    """Text-Based User Interface for HANGMAN."""
+    pass
+
+
+@tui.command()
+def tui_settings():
+    pass
+
+
+@tui.command()
+def play_tui():
+    tui_graphics.clear()
+    click.echo(tui_graphics.banner)
+
+    lives = 8
+    while lives > 0:
+        current_hangman_drawing = tui_graphics.get_hangman_drawing(lives)
+        click.echo(current_hangman_drawing)
+        lives -= 1
+
+
+@main.group()
+def gui():
+    """Graphical User Interface for HANGMAN"""
+    pass
+
+
+@gui.command()
+def launch_gui():
+    pass
+
+
+if __name__ == "__main__":
+    main()
